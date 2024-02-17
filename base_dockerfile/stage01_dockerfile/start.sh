1da89f621f47c730ce34f98e383bc15e8d027270
Leekb0804 <lkb8713@gmail.com>
2024-02-17 06:06:50 +0000

    ssh"

---
+++ base_dockerfile/stage01_dockerfile/start.sh
@@ -0,0 +1,19 @@
+
+
+
+echo "Welcome to POL"
+echo " "
+echo -e "\x1b[38;2;255;0;0m ________  ________  ___ \x1b[0m "
+echo -e "\x1b[38;2;255;69;0m|\   __  \|\   __  \|\  \ \x1b[0m"
+echo -e "\x1b[38;2;255;105;0m\ \  \|\  \ \  \|\  \ \  \ \x1b[0m"
+echo -e "\x1b[38;2;255;165;0m \ \   ____\ \  \ \  \ \  \ \x1b[0m"
+echo -e "\x1b[38;2;255;195;0m  \ \  \___|\ \  \_\  \ \  \____  \x1b[0m"
+echo -e "\x1b[38;2;255;225;0m   \ \__\    \ \_______\ \_______\ \x1b[0m"
+echo -e "\x1b[38;2;255;255;0m    \|__|     \|_______|\|_______| \x1b[0m"
+echo " "
+echo $stage
+echo " "
+echo "You can learn [ ls ] and [ option ]"
+
+cd stage01
+#chmod 700 /home
