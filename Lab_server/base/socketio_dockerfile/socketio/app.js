1bc2953483f3df135a0858af471a51cf4dd9f974
JeongRae <kkwjdfo@gmail.com>
2024-04-05 00:24:42 +0900

    [Feat] health check 기능 추가

--- Lab_server/base/socketio_dockerfile/socketio/app.js
+++ Lab_server/base/socketio_dockerfile/socketio/app.js
@@ -8,9 +8,11 @@ const io = socketIo(server);
 
 const socketRouter = require("./src/routes/socketRouter");
 const stageRouter = require("./src/routes/stageRouter");
+const healthChecker = require("./src/routes/healthChecker");
 
 app.use(express.json());
 app.use(stageRouter);
+app.use(healthChecker);
 
 socketRouter(io);
 
